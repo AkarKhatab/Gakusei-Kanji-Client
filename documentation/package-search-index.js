6384cee9fd85f23cf989f023107f2e287f3eb0ec
Akar Khatab <Akar.Khatab@gmail.com>
2018-11-23 13:09:42 +0100

    Generated javadocs.

---
+++ documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"se.kits.gakusei.kanjiclient"},{"l":"se.kits.gakusei.kanjiclient.dto"},{"l":"se.kits.gakusei.kanjiclient.service"}]
---
+++ documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"se.kits.gakusei.kanjiclient","l":"KanjiClientApplication"},{"p":"se.kits.gakusei.kanjiclient.dto","l":"KanjiDrawingDTO"},{"p":"se.kits.gakusei.kanjiclient.service","l":"KanjiService"},{"p":"se.kits.gakusei.kanjiclient.service","l":"ShellService"}]
